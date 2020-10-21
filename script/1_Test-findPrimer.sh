3db0f43f8ef62846af11d138a576ff2c2a1e70c4
amonjot <amonjot@gmail.com>
2020-10-21 09:32:00 +0200

    add test primer scripts

---
+++ script/1_Test-findPrimer.sh
@@ -0,0 +1,67 @@
+#                                 _       _
+#     /\                         (_)     | |
+#    /  \   _ __ ___   ___  _ __  _  ___ | |_
+#   / /\ \ | '_ ` _ \ / _ \| '_ \| |/ _ \| __|
+#  / ____ \| | | | | | (_) | | | | | (_) | |_
+# /_/    \_\_| |_| |_|\___/|_| |_| |\___/ \__|
+#                               _/ |
+#                              |__/
+# 24/01/2020
+#
+#!/bin/bash
+
+#Ask V4 or V9
+echo Enter ribosomal region (V4 or V9) :
+read region
+if [ $region == "V4" ]
+then
+## Boucle for
+    cd ..
+    cp -r dataPANAM/V4 dataPANAM/V4-testPrimer
+
+    for f in $(echo dataPANAM/V4-testPrimer)
+    do
+        for fichier in $(ls $f/*_R1_*.fastq)
+        do
+            echo $fichier
+            reverse=$(echo $fichier | sed 's/R1/R2/g')
+            echo $reverse
+        
+            label=$(echo $fichier | awk -F"/" '{ print $3}' |sed -E 's/(F.*_)S.*.fastq$/\1/')
+            echo $label
+            
+    #Assemblage
+            vsearch -fastq_mergepairs $fichier -reverse $reverse -fastq_maxmergelen 500 -fastq_minlen 200 -fastq_minovlen 50 -fastq_maxdiffs 0 -fastqout $f/assemble.fastq
+    #filter
+            vsearch -fastq_filter $f/assemble.fastq -fastaout $f/assemble.fasta -relabel $label\_ -fasta_width 0
+    #chimère
+            vsearch -uchime_denovo $f/assemble.fasta -nonchimeras $f/nonchimera-$label.fasta -fasta_width 0
+        done
+    done
+fi
+if [ $region == "V9" ]
+then
+    ## Boucle for
+    cd ..
+    cp -r dataPANAM/V9 dataPANAM/V9-testPrimer
+
+    for f in $(echo dataPANAM/V9-testPrimer)
+    do
+        for fichier in $(ls $f/*_R1_*.fastq)
+        do
+            echo $fichier
+            reverse=$(echo $fichier | sed 's/R1/R2/g')
+            echo $reverse
+        
+            label=$(echo $fichier | awk -F"/" '{ print $3}' |sed -E 's/(F.*_)S.*.fastq$/\1/')
+            echo $label
+            
+    #Assemblage
+            vsearch -fastq_mergepairs $fichier -reverse $reverse -fastq_maxmergelen 300 -fastq_minlen 100 -fastq_minovlen 50 -fastq_maxdiffs 0 -fastqout $f/assemble.fastq
+    #filter
+            vsearch -fastq_filter $f/assemble.fastq -fastaout $f/assemble.fasta -relabel $label\_ -fasta_width 0
+    #chimère
+            vsearch -uchime_denovo $f/assemble.fasta -nonchimeras $f/nonchimera-$label.fasta -fasta_width 0
+        done
+    done
+fi
---
+++ script/2_Sort-onlyPrimer.sh
@@ -0,0 +1,46 @@
+#                                 _       _
+#     /\                         (_)     | |
+#    /  \   _ __ ___   ___  _ __  _  ___ | |_
+#   / /\ \ | '_ ` _ \ / _ \| '_ \| |/ _ \| __|
+#  / ____ \| | | | | | (_) | | | | | (_) | |_
+# /_/    \_\_| |_| |_|\___/|_| |_| |\___/ \__|
+#                               _/ |
+#                              |__/
+# 27/02/2020
+#
+#!/bin/bash
+
+#Ask V4 or V9
+echo Enter ribosomal region (V4 or V9) :
+read region
+if [ $region == "V4" ]
+then
+## Sort primers
+    cd ../dataPANAM
+    for f in $(ls V4-testPrimer/nonchimera-*.fastq.fasta)
+    do
+        x=$(grep "^TTGG[CT][AG]AATGCTTTCGC.*TACCGCGGC[GT]GCTG[AG]CAC$" $f | wc -l)
+        y=$(grep "^GTG[CT]CAGC[AC]GCCGCGGTA.*GCGAAAGCATT[CT][AG]CCAA$" $f | wc -l)
+        w=$(cat $f | grep -v ">" | grep "N" | wc -l)
+        z=$(echo $(($x+$y)))
+        m=$(grep "^>" $f | wc -l)
+        n=$(($z*100/$m))
+        echo $f "- Sequences with primers : "$z "-" $n"%" "of total sequences -" $m "- and" $w "sequences with Ns" | cut -d"/" -f2 | sed 's/nonchimera-//g' |sed 's/_2_R1_clean.fastq.fasta//g' | tee -a V4-testPrimer/V4-primer.out
+    done
+fi
+if [ $region == "V9" ]
+then
+    cd ../dataPANAM
+    for f in $(ls V9-testPrimer/nonchimera-*.fastq.fasta)
+    do
+        x=$(grep "^TTGTACACACCGCCC.*GTAGGTGAACCTGC[AG]GAAGG$" $f | wc -l)
+        y=$(grep "^CCTTC[CT]GCAGGTTCACCTAC.*GGGCGGTGTGTACAA$" $f | wc -l)
+        w=$(cat $f | grep -v ">" | grep "N" | wc -l)
+        z=$(echo $(($x+$y)))
+        m=$(grep "^>" $f | wc -l)
+        n=$(($z*100/$m))
+        echo $f "- Sequences with primers : "$z "-" $n"%" "of total sequences -" $m "- and" $w "sequences with Ns" | cut -d"/" -f2 | sed 's/nonchimera-//g' |sed 's/_1_R1_clean.fastq.fasta//g' | tee -a V9-testPrimer/V9-primer.out
+    done
+fi
+    
+
