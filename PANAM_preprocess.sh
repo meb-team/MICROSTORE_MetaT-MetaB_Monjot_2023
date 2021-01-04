79fa270088c19536536e240cb27343ecf87986dd
amonjot <amonjot@gmail.com>
2021-01-04 14:44:19 +0100

    add vsearch 2.15.1 utilisation

--- PANAM_preprocess.sh
+++ PANAM_preprocess.sh
@@ -20,8 +20,8 @@ bash 2_Install_assembler.sh
 # Use Vsearch v2.15.1
 cd ../dataPANAM
 mv PANAM2/bin/vsearch PANAM2/bin/vsearch-1.9.9/bin/
-cp ../bin/vsearch-2.15.1/ PANAM2/bin/
-mv PANAM2/bin/vsearch-2.15.1/bin/vsearch PANAM2/bin/
+cp -r ../bin/vsearch-2.15.1/ PANAM2/bin/
+cp ../bin/vsearch PANAM2/bin/
 
 # End
 echo "Preprocess and assembler installation finish !"
