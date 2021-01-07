061a75147e69e05cf6c1797d633cdd159a1802e6
amonjot <amonjot@gmail.com>
2021-01-07 17:51:02 +0100

    update functionnal diversity script

--- script/9-5_Analyse_Fonctionelle.R
+++ script/9-5_Analyse_Fonctionelle.R
@@ -7,9 +7,9 @@
 #                               _/ |
 #                              |__/
 # !/usr/bin/env Rscript
-# XX/XX/XXXX
+# 06/01/2021
 #
-# Script Composition
+# Script to generate taxonomic profile table
 # Set directory, input and output -----------------------------------------------------------
 # Detect R or Rstudio
 se <- Sys.getenv()
@@ -49,35 +49,35 @@ BTT_table <- read.csv(file = "../../rawdata/BTTwellannoted.csv", sep = ";")
 
 # Enter mode and Group to study -------------------------------------------
 
-if (length(args)==2) {
+if (length(args)==1) {
   cat("Enter mode (Superphylum or Phylum or Class) : ");
   Mode <- readLines("stdin",n=1);
   cat("You entered")
   str(Mode);
   cat( "\n" )}
-if (length(args)>2) {
-  Mode <- args[9]
+if (length(args)>1) {
+  Mode <- args[2]
 }
 
 if (Mode == "Phylum") {
-  if (length(args)==2) {
+  if (length(args)==1) {
     cat("Enter Phylum (Fungi, Alveolata, etc) : ");
     Group <- readLines("stdin",n=1);
     cat("You entered")
     str(Group);
     cat( "\n" )}
-  if (length(args)>2) {
-    Group <- args[10]}
+  if (length(args)>1) {
+    Group <- args[3]}
 }
 if (Mode == "Class") {
-  if (length(args)==2) {
+  if (length(args)==1) {
     cat("Enter Class (Bacillariophyta, Synurophyceae, etc) : ");
     Group <- readLines("stdin",n=1);
     cat("You entered")
     str(Group);
     cat( "\n" )}
-  if (length(args)>2) {
-    Group <- args[10]}
+  if (length(args)>1) {
+    Group <- args[3]}
 }
 # Theme unique Dark perso -------------------------------------------------------
 theme_unique_dark <- function (base_size = 12, base_family = "") {
@@ -143,19 +143,52 @@ tax_table <- tableVinput %>% select(Taxonomy)
 tax_table <- separate(tax_table, all_of(Taxonomy), c("Domain","Superphylum","Phylum","Class","Order","Family","Genus","Species","Taxo9","Taxo10","Taxo11"), sep =";")
 tax_table[tax_table=='NA'] <- NA
 # Create taxonomix profiles ---------------------------------------------------------------
+## Prepare taxonomic profiles with tableVinput 
 profile_tax_table <- as.data.frame(unique(tableVinput$Taxonomy)) ; colnames(profile_tax_table) <- "Taxonomy"
 profile_tax_table <- as.data.frame(profile_tax_table[order(profile_tax_table$Taxonomy),]) ; colnames(profile_tax_table) <- "Taxonomy"
 profile_tax_table <- separate(profile_tax_table, all_of(Taxonomy), c("Domain","Superphylum","Phylum","Class","Order","Family","Genus","Species","Taxo9","Taxo10","Taxo11"), sep =";")
 profile_tax_table[profile_tax_table=='NA'] <- NA
 profile_tax_table[,"Last"] <- NA
-exept <- c("environmental samples", "environvironmental samples")
+## Input exeption for "Last" Annotation
+exept <- c("environmental samples", "environvironmental samples", "unclassified", "unclassified")
+## Remove exeption if Last annotation exist
+for (i in as.numeric(row.names(profile_tax_table))) {
+  w <- length(profile_tax_table[i,][is.na(profile_tax_table[i,]) != TRUE])
+  if ( length(grep(profile_tax_table[i,w],pattern = paste(exept,collapse="|"))) == 1 ) { 
+    pattern1 <- noquote(colnames(profile_tax_table[w-1]))
+    pattern2 <- profile_tax_table[i,w-1]
+    x <- profile_tax_table %>% filter(!!sym(pattern1) %in% !!pattern2)
+    if ( nrow(x) > 1 ) { 
+      if (length(grep(is.na(x[,8]), pattern = "TRUE")) > 0 ) { profile_tax_table[i,"Last"] <- "NOK"}}}}
+profile_tax_table <- profile_tax_table %>% filter(is.na(Last) == TRUE)
+## If Last annotation doesn't exist with exeption, use prior annotation
 for (i in row.names(profile_tax_table)) {
  w <- length(profile_tax_table[i,][is.na(profile_tax_table[i,]) != TRUE])
- if ( length(grep(profile_tax_table[i,w],all_of(exept))) == 1 ) { profile_tax_table[i,"Last"] <- profile_tax_table[i,w-1] }
+ if ( length(grep(profile_tax_table[i,w],pattern = paste(exept,collapse="|"))) == 1 ) { 
+   if ( length(grep(profile_tax_table[i,w-1],pattern = paste(exept,collapse="|"))) == 1 ) { profile_tax_table[i,"Last"] <- profile_tax_table[i,w-2] }
+   else { profile_tax_table[i,"Last"] <- profile_tax_table[i,w-1] }}
  else { profile_tax_table[i,"Last"] <- profile_tax_table[i,w] }
 }
+## Join BTT table (Ramond et al., 2018)
 profile_tax_table <- left_join(x = profile_tax_table, y = BTT_table %>% select(Last,SizeMin,SizeMax,Cover,Shape,Spicule,Symmetry,Polarity,Colony,Motility,Chloroplast,Plast_Origin,Ingestion,Symbiontic,Zoosporic_Parasite,Cyst_Spore), by = "Last")
-
-
-
+## Join size annotation for multiple Last annotation after BTT binding
+h <- 0
+while ( h < 3) { h <- h+1
+i <- 1
+  while (i < nrow(profile_tax_table)-1) {
+    i <- i+1
+    j <- i+1
+    if (profile_tax_table[i,"Last"] == profile_tax_table[j,"Last"]) { 
+      profile_tax_table[i:j,"SizeMin"] <- min(c(profile_tax_table[i,"SizeMin"],profile_tax_table[j,"SizeMin"]))
+      profile_tax_table[i:j,"SizeMax"] <- max(c(profile_tax_table[i,"SizeMax"],profile_tax_table[j,"SizeMax"]))}
+  }
+}
+## remove doublons
+profile_tax_table <- distinct(profile_tax_table)
+y <- length(unique(profile_tax_table$Last))
+x <- 0
+for (i in unique(profile_tax_table$Last)) { if (length(grep(pattern = i, BTT_table$Last)) > 0) { x<- x+1}}
+z <- x*100/y
+z
+write.table(profile_tax_table, file = "OTU-Table/Taxonomic_Profil_Table.csv", sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
 
